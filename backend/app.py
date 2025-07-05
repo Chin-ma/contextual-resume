802be73e716afff8f11ffe18a38af1f575ad2e86
Chin-ma <chinmay2003cp@gmail.com>
2025-07-05 13:05:50 +0530

    Updated app.py for Gunicorn

--- backend/app.py
+++ backend/app.py
@@ -32,6 +32,8 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True)
+    # app = create_app()
+    app.run(debug=True, port=5000)
